20e70d75185b6050f86d7d153deca07f7adbf591
wbw <wbw262174@gmail.com>
2022-03-23 14:18:28 +0800

    [add]1.整合redis

---
+++ src/main/java/org/wbw/example/controller/RedisController.java
@@ -0,0 +1,17 @@
+package org.wbw.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wbw
+ * @description: TODO
+ * @date 2022-3-23 12:34
+ */
+@Slf4j
+@RestController
+@RequestMapping("redis")
+public class RedisController {
+
+}
